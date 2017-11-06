f79b7032c8be040380fa0feea2869832331d3b06
Maxim Tyulenev <mr_makss@mail.ru>
2017-11-06 21:15:19 +0700

    ref

--- frontend/src/app/page/menu/menu.component.ts
+++ frontend/src/app/page/menu/menu.component.ts
@@ -5,4 +5,17 @@ import { Component } from '@angular/core';
   templateUrl: './menu.component.html'
 })
 export class MenuComponent {
+  
+  toggle_dropdown_menu = true;
+  hamburder_icon_box = 'hamburder_icon_box';
+  
+  toggle_menu(): void {
+    if(this.hamburder_icon_box == 'hamburder_icon_box') {
+      this.toggle_dropdown_menu = false;
+      this.hamburder_icon_box = 'hamburder_icon_box open';
+    }else {
+      this.toggle_dropdown_menu = true;
+      this.hamburder_icon_box = 'hamburder_icon_box';
+    }
+  }
 }
