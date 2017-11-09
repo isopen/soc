ec5f4bec6adfbfa2709b2d647a1d2a356549b765
Maxim Tyulenev <mr_makss@mail.ru>
2017-11-10 00:52:58 +0700

    ref

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Backend
   class Application < Rails::Application
+    
     config.api_only = true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
@@ -15,6 +16,8 @@ module Backend
     config.generators do |g|
       g.orm :mongoid
     end
+    
+    config.host = "http://localhost:3000"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
--- backend/test/controllers/auth_controller_test.rb
+++ backend/test/controllers/auth_controller_test.rb
@@ -2,13 +2,13 @@ class AuthControllerTest < ActionDispatch::IntegrationTest
   
   test "auth_login" do
 
+    # parameters for forming combinations
     r = [{"login" => ""}, {"token" => ""}, {"password" => ""}, {"guid" => ""}]
-    
+    # generate combinations and send post requests  
     Gen::Data.request_data(r).map do |params|
-      response = RestClient.post "http://localhost:3000/login", params
-      p response
+      response = RestClient.post (Backend::Application.config.host + "/login"), params
+      assert_equal false, JSON.parse(response)["success"]
     end
-    assert true
   end
   test "auth_reg" do
     assert true
