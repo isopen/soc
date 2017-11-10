75bf14eb922df69bec507fe9d197d193cfdd9a96
Maxim Tyulenev <mr_makss@mail.ru>
2017-11-10 21:07:48 +0700

    ref

--- frontend/src/app/auth/auth.module.ts
+++ frontend/src/app/auth/auth.module.ts
@@ -1,10 +1,6 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { CommonModule }  from '@angular/common';
 import { FormsModule }   from '@angular/forms';
 
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule, Routes } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 
 import { AuthComponent } from './auth.component';
@@ -15,8 +11,6 @@ import { AuthService } from './auth.service';
 
 @NgModule({
   imports: [
-    CommonModule,
-    HttpClientModule,
     FormsModule
   ],
   declarations: [
