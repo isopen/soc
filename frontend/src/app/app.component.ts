6a955f842ed5443957cb09bf47f3a7f425f5e0c8
Maxim Tyulenev <mr_makss@mail.ru>
2017-11-10 18:22:15 +0700

    ref

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,20 +1,7 @@
 import { Component} from '@angular/core';
 
-import { Ng2Cable, Broadcaster } from 'ng2-cable';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
-  constructor(private ng2cable: Ng2Cable, private broadcaster: Broadcaster) {
-    /*this.ng2cable.subscribe('http://localhost:28080/chat', 'ChatChannel', { room: 'My room'});
-    //By default event name is 'channel name'. But you can pass from backend field { action: 'MyEventName'}
-
-    this.broadcaster.on<string>('ChatChannel').subscribe(
-      message => {
-        console.log(message);
-      }
-    );*/
-  }
-}
+export class AppComponent {}
--- frontend/src/app/auth/auth.component.ts
+++ frontend/src/app/auth/auth.component.ts
@@ -1,7 +1,18 @@
 import { Component } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './auth.component.html'
 })
 export class AuthComponent {
+  constructor(
+    private cookieService: CookieService,
+    private router: Router
+  ) {
+    if(this.cookieService.check("_guid") && this.cookieService.check("_login") && this.cookieService.check("_token")) {
+      this.router.navigateByUrl('/page/' + this.cookieService.get("_guid"));
+    }
+  }
 }
