46c56670ab4674760c4285cc8febe02296fdeafd
Maxim Tyulenev <mr_makss@mail.ru>
2017-12-19 21:10:38 +0700

    ref

--- backend/app/channels/page_channel.rb
+++ backend/app/channels/page_channel.rb
@@ -8,7 +8,7 @@ class PageChannel < ApplicationCable::Channel
 
   def send_message(data)
     ActionCable.server.broadcast(
-        data[:user_id],
+        data['user_id'],
         type: 'send_message',
         message: data['message']
     )
@@ -16,7 +16,7 @@ class PageChannel < ApplicationCable::Channel
 
   def send_message_to_wall(data)
     ActionCable.server.broadcast(
-        data[:user_id],
+        data['user_id'],
         type: 'update_wall',
         message: data['message']
     )
