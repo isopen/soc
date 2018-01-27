7bb4c4d39c47b8ef509f20b4a039ec989ce04ad9
Maxim Tyulenev <mr_makss@mail.ru>
2018-01-27 23:05:52 +0700

    ref

--- frontend/src/app/page/wall/wall.component.ts
+++ frontend/src/app/page/wall/wall.component.ts
@@ -1,5 +1,6 @@
 import { Renderer2, Component, ElementRef, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 import { PageService } from '../page.service';
 import { ConfigService } from '../../app.config';
@@ -11,6 +12,7 @@ import { UtilsService } from '../../utils/app.utils';
 })
 export class WallComponent implements OnInit {
   constructor(
+    private router: Router,
     private renderer: Renderer2,
     private el: ElementRef,
     private pageService: PageService,
@@ -69,6 +71,10 @@ export class WallComponent implements OnInit {
 
   }
 
+  transition_to_wall(guid: string): void {
+    this.router.navigateByUrl('/');
+  }
+
   gen_im_message_wrap(data, selector, type): void {
 
     data.message.text = this.renderer.createText(data.message.text);
@@ -111,6 +117,9 @@ export class WallComponent implements OnInit {
     this.renderer.appendChild(im_content_message_wrap, im_message_meta);
 
     this.renderer.appendChild(im_message_author, data.message.author);
+    this.renderer.listen(im_message_author, 'click', (event) => {
+      this.transition_to_wall(data.message.author.textContent);
+    });
     this.renderer.appendChild(im_message_author_wrap, im_message_author);
     this.renderer.appendChild(im_message_body, im_message_author_wrap);
 
